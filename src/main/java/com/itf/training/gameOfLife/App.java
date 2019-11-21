2481b70af6315abc63c12a4548fd921766b5079b
Florian <florian_dittrich@hotmail.de>
2019-11-21 09:44:16 +0100

    first commit

---
+++ src/main/java/com/itf/training/gameOfLife/App.java
@@ -0,0 +1,13 @@
+package com.itf.training.gameOfLife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
